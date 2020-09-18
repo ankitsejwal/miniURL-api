9c9c979db073324113285e1433a663e50e4397b1
ankitsejwal <ankit@sejw.al>
2020-09-19 03:05:03 +1000

    finished all the functionality

--- server.js
+++ server.js
@@ -27,4 +27,15 @@ app.post("/shorturls", async (req, res) => {
   res.redirect("/");
 });
 
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  console.log(shortUrl);
+  if (shortUrl === null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  await shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
 app.listen(process.env.PORT || 5000);
