9b5f15236384199a70dc545345d45733e776368d
ankitsejwal <ankit@sejw.al>
2020-09-19 00:39:08 +1000

    create model to store urls

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+
+mongoose
+  .connect("mongodb://localhost/urlShortener", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("database connected"))
+  .catch((err) => console.error(err));
+
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls, shortUrls });
+});
+
+app.post("/shorturls", async (req, res) => {
+  await ShortUrl.create({
+    full: req.body.fullUrl,
+  });
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 5000);
