fedec0718304d087c6394318779c7ee3de0a9476
ankit sejwal <ankit@sejw.al>
2023-06-25 11:57:27 +0000

    updated Url model

--- models/Url.js
+++ models/Url.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const urlSchema = new mongoose.Schema({
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   shortUrl: { type: String, unique: true, required: true },
   fullUrl: { type: String, unique: true, required: true },
   clicks: { type: Number, default: 0 },
 });
 
 const joiUrlSchema = {
-  createdBy: Joi.ObjectId(),
+  user: Joi.ObjectId(),
   full: Joi.string().trim().uri().min(1),
+  customUrl: Joi.boolean().default(false),
+  shortUrlLength: Joi.number().min(3).max(10).default(5),
 };
 
 urlSchema.methods.validateData = function (data) {
