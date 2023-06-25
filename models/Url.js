9f9b4300cc8150410f37b1a1fe573c3a9d36c029
ankit sejwal <ankit@sejw.al>
2023-06-25 14:28:40 +0000

    updated models

--- models/Url.js
+++ models/Url.js
@@ -5,14 +5,16 @@ const urlSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   shortUrl: { type: String, unique: true, required: true },
   fullUrl: { type: String, unique: true, required: true },
+  customUrl: { type: Boolean, default: false },
   clicks: { type: Number, default: 0 },
+  collisions: { type: Number, default: 0 },
 });
 
 const joiUrlSchema = {
   user: Joi.ObjectId(),
   full: Joi.string().trim().uri().min(1),
   customUrl: Joi.boolean().default(false),
-  shortUrlLength: Joi.number().min(3).max(10).default(5),
+  shortUrlLength: Joi.number().min(3).max(10).default(4),
 };
 
 urlSchema.methods.validateData = function (data) {
