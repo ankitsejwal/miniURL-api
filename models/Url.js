e2228f901e809ed0956e89d7cfcf67d4b1e91c14
ankit sejwal <ankit@sejw.al>
2023-06-25 16:57:59 +0000

    implemented url generation using static method

--- models/Url.js
+++ models/Url.js
@@ -1,20 +1,39 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+const nanoid = require('nanoid');
 
 const urlSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   shortUrl: { type: String, unique: true, required: true },
   fullUrl: { type: String, unique: true, required: true },
   customUrl: { type: Boolean, default: false },
-  clicks: { type: Number, default: 0 },
+  visits: { type: Number, default: 0 },
   collisions: { type: Number, default: 0 },
 });
 
 const joiUrlSchema = {
   user: Joi.ObjectId(),
-  full: Joi.string().trim().uri().min(1),
+  fullUrl: Joi.string().trim().uri().min(1),
   customUrl: Joi.boolean().default(false),
   shortUrlLength: Joi.number().min(2).max(10).default(4),
+};
+
+urlSchema.static.createShortUrl = async function (shortUrlLength) {
+  let shortUrl;
+  let url;
+  let unique = false;
+  let collision = 0;
+  do {
+    shortUrl = nanoid(shortUrlLength);
+    // look for collision
+    url = await Url.findOne({ shortUrl: shortUrl });
+    if (url) {
+      collision++;
+    } else {
+      unique = true;
+    }
+  } while (!unique);
+  return { shortUrl: shortUrl, collision: collision };
 };
 
 const Url = mongoose.model('Url', urlSchema);
--- routes/urls.js
+++ routes/urls.js
@@ -23,7 +23,17 @@ router.get('/:id', validateSchema, async (req, res) => {
 
 router.post('/', validateSchema, async (req, res) => {
   try {
-    const url = new Url(req.value);
+    const fullUrl = req.value.fullUrl;
+    let url = await Url.findOne({ fullUrl: fullUrl });
+    // if fullUrl already exists return the saved shortUrl
+    if (url) return res.status(200).send(url.shortUrl);
+
+    const { shortUrl, collision } = await Url.createShortUrl(req.value.shortUrlLength);
+    req.value.shortUrl = shortUrl;
+    req.value.collision = collision;
+
+    // add short url to value object
+    url = new Url(req.value);
     await url.save();
   } catch (error) {
     res.status(400).send(error);
