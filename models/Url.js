24c16cdf2a870517b7f630c8681d206367b88048
ankitsejwal <ankit@sejw.al>
2023-07-03 17:01:47 +0530

    bug fixes

--- models/Url.js
+++ models/Url.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectid = require('joi-objectid')(Joi);
 const { nanoid } = require('nanoid');
 
 const urlSchema = new mongoose.Schema({
@@ -12,7 +13,7 @@ const urlSchema = new mongoose.Schema({
 });
 
 const joiUrlSchema = {
-  user: Joi.ObjectId(),
+  user: Joi.objectid(),
   fullUrl: Joi.string().trim().uri().min(1),
   customUrl: Joi.boolean().default(false),
   shortUrlLength: Joi.number().min(2).max(10).default(4),
@@ -27,11 +28,8 @@ urlSchema.static.createShortUrl = async function (shortUrlLength) {
     shortUrl = nanoid(shortUrlLength);
     // look for collision
     url = await Url.findOne({ shortUrl: shortUrl });
-    if (url) {
-      collision++;
-    } else {
-      unique = true;
-    }
+    if (url) collision++;
+    else unique = true;
   } while (!unique);
   return { shortUrl: shortUrl, collision: collision };
 };
