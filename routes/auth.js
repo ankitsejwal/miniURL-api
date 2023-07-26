0fdbfe55204b70102ab8ba91d770a63a04727903
ankitsejwal <ankit@sejw.al>
2023-07-27 00:09:05 +0530

    fixed issue with encryption on auth and user

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,8 @@ router.post('/', validate(joiAuthSchema), async (req, res) => {
   const errorMessage = 'username and password combination is incorrect';
   let user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(401).json(errorMessage);
-  // const match = await bcrypt.compare(req.body.password, req.body.password);
-  if (req.body.password !== user.password) return res.status(401).json(errorMessage);
+  const match = await bcrypt.compare(req.body.password, user.password);
+  if (!match) return res.status(401).json(errorMessage);
 
   // if user is authenticated send token in header
   const token = user.genAuthToken();
